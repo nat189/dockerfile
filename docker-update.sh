1c53f6466464f85325dfacf869a6017225bde35e
nat189 <63495071+nat189@users.noreply.github.com>
2022-02-26 15:15:13 +0700

    Update docker-update.sh

--- docker-update.sh
+++ docker-update.sh
@@ -2,9 +2,14 @@
 docker run --rm \
 -v /var/run/docker.sock:/var/run/docker.sock \
 containrrr/watchtower \
---run-once emby
+--run-once emby1
 sleep30
 docker run --rm \
 -v /var/run/docker.sock:/var/run/docker.sock \
 containrrr/watchtower \
---run-once plex
+--run-once plex5
+sleep30
+docker run --rm \
+-v /var/run/docker.sock:/var/run/docker.sock \
+containrrr/watchtower \
+--run-once plex1
